524e249273051e0423f083796c7c1ef2808ecdca
lsolovy <solovy.louis@gmail.com>
2021-11-21 18:36:05 -0600

    added a basic frontend UI login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,62 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import "./mystyles.scss"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <><nav class="navbar is-black" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <StaticImage src="../images/Iowa_Hawkeyes_logo.svg.png" width="50" height="28"/>
+          </a>
+      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Hawkeye Polling
+            </a>
+          </div>
+        </div>
+    </nav>
+    <section class="hero is-warning is-fullheight">
+        <div class="hero-body">
+          <div class="container">
+            <div class="columns is-centered">
+              <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+                <form action="" class="box">
+                  <div class="field">
+                    <label for="" class="label">Email</label>
+                      <input type="email" placeholder="e.g. bobsmith@gmail.com" class="input" required />
+                  </div>
+                  <div class="field">
+                    <label for="" class="label">Password</label>
+                      <input type="password" placeholder="*******" class="input" required />
+                  </div>
+                  <div class="field">
+                    <label for="" class="checkbox">
+                      <input type="checkbox" />
+                    Remember me
+                    </label>
+                  </div>
+                  <div class="field">
+                    <button class="button is-warning">
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section></>
+  )
+}
 
 export default IndexPage
