a1bcef888e85544bf920d66239dba5958b8b16a5
lhagan97 <liam-hagan@uiowa.edu>
2021-12-02 12:41:10 -0600

    added client-get.js

---
+++ client-get.js
@@ -0,0 +1,47 @@
+// ------------ NodeJS runtime ---------------
+// Add aws-sdk in package.json as a dependency
+// Example:
+// {
+//     "dependencies": {
+//         "aws-sdk": "^2.0.9",
+//     }
+// }
+// Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
+// Format of the above file should be:
+//  [default]
+//  aws_access_key_id = YOUR_ACCESS_KEY_ID
+//  aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+
+const AWS = require('aws-sdk');
+
+
+function createDynamoDbClient() {
+  // Set the region
+  AWS.config.update({
+    accessKeyId: 'AKIAQAUYDWTW6BQE2UFZ',
+    secretAccessKey: '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',
+    region: 'us-east-1'
+})
+  return new AWS.DynamoDB();
+}
+
+
+function returnData(UniqueTitle){
+
+    const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+    dynamoDB
+    .get({
+        TableName: "SeniorDesignLab3DB",
+        Key: {
+        'UniqueTitle': UniqueTitle
+        },
+    })
+    .promise()
+    .then(data => console.log(data.Item))
+    .catch(console.error)
+}
+
+createDynamoDbClient();
+var response = returnData('yee')
+console.log(response)
